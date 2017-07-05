5e1a5be2dde9029afc35ca3a3de6b26426ebbdee
Nicacioneto <nicacionetobsb@hotmail.com>
2017-07-05 14:21:52 -0300

    Fix templates and urls

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,6 +6,6 @@ app_name = 'rooms'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<room_id>[0-9]+)/$', views.show, name='show'),
-    url(r'^room/new/$', views.new, name = 'new'),
+    url(r'^new/$', views.new, name = 'new'),
 
 ]
